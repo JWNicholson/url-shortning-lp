1e4b341b0a422497dae141a8fcdd61e67b5299b9
JW Nicholson <john.nicholson812@gmail.com>
2020-10-01 14:53:04 -0400

    screen size > 768 navbar styles done. needs small screen version

--- src/components/Nav/Navbar.js
+++ src/components/Nav/Navbar.js
@@ -7,16 +7,20 @@ const Navbar = () => {
             <div className="left-side">
                 <a href="/" className="logo"><h1>Shortly</h1></a>
 
-                <div className="left-links">
+                <div className="left-side">
+                    <div className="left-side-links">
                     <a href="/">Features</a>
                     <a href="/">Pricing</a>
                     <a href="/">Resources</a>
                 </div>
+                </div>
             </div>
 
             <div className="right-side" >
+            <div className="right-side-links">
                 <a href="/">Login</a>
                 <button className="right-btn">Sign Up</button>
+            </div>
             </div>
         </nav>
     )
